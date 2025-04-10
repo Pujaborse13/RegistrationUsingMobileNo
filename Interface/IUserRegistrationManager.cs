54ac2528cfeaefde3fe6d38e0f516883955e099c
Puja Borse <107254941+Pujaborse13@users.noreply.github.com>
2025-04-11 00:24:55 +0530

    Add files via upload

---
+++ Interface/IUserRegistrationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RepositoryLayer.Migrations;
+using System.Threading.Tasks;
+using CommonLayer.Models;
+
+namespace ManagerLayer.Interface
+{
+    public interface IUserRegistrationManager
+    {
+        public Task<string> RegisterUserUsingMobileNo(string mobileNo);
+        public Task<bool> SendOTP(string mobileNo);
+
+        public Task<bool> VerifyOTP(string mobileNo, string otp);
+
+
+
+
+
+    }
+}
