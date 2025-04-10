2919a2232423dd0e31d425c319f081f570bd4a95
Puja Borse <107254941+Pujaborse13@users.noreply.github.com>
2025-04-11 00:33:02 +0530

    CommonLayer

---
+++ UserRegistrationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class UserRegistrationModel
+    {
+        
+
+        public string MobileNo { get; set; }
+
+
+    }
+}
